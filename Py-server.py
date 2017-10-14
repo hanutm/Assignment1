a7a24a64e6e7b5264349c314cf8b149aa6014a18
Hanut Mathur <hanut.mathur@live.in>
2017-10-14 16:00:52 +0100

    Server Analysing initial message from client

--- Py-server.py
+++ Py-server.py
@@ -3,20 +3,38 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 host = socket.gethostname()
-
 port = 50000
-
 server.bind((host.port))
-
 server.listen(5)
 
+chatroomname = {}
+client = {}
 while True:
 	
 	csock, addr = server.accept()
-	
-	print("Connected with %s" % str(addr))
+
+	#monitoring connections
+	print("Connected to %s",str(addr))
+
+	#processing response
+	while True:
+		conn_msg = csock.recv(1024)	#receiving 1024 bytes of data at a time (for testing)
+		if not data: break
+		
+		#check incoming message
+		print("Message received :", conn_msg
+		
+		#formatting incoming data
+		rname_start = data.find('JOIN_CHATROOM')+3          #Chatroom Name
+	 	rname_end = data.find('\n')-2
+		chatroomname[0] = data[rname_start:rname_end]
+
+		cname_start = data.find('CLIENT_NAME')+3	    #Client Name		
+		cname_end = data.find('\n',cname_start)-2
+		client[0] = data[cname_start:cname_end]
+			
+		#No other parameters to check because of TCP connection
 
 	response = "Joined chatroom"
 	csock.send(response.encode('ascii'))
