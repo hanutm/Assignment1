2cc62a81424dd7017c00d32e109b6d5fc1f1d5ca
Hanut Mathur <hanut.mathur@live.in>
2017-10-14 21:53:10 +0100

    Working Single Thread Server

--- Py-server.py
+++ Py-server.py
@@ -5,9 +5,9 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 50000
-server.bind((host.port))
-server.listen(5)
-
+server.bind((host,port))
+server.listen(0)
+print(host)
 chatroomname = {}
 client = {}
 while True:
@@ -20,23 +20,24 @@ while True:
 	#processing response
 	while True:
 		conn_msg = csock.recv(1024)	#receiving 1024 bytes of data at a time (for testing)
-		if not data: break
+		if not conn_msg: break
 		
 		#check incoming message
-		print("Message received :", conn_msg
+		print("Message received :", conn_msg)
 		
 		#formatting incoming data
-		rname_start = data.find('JOIN_CHATROOM')+3          #Chatroom Name
-	 	rname_end = data.find('\n')-2
-		chatroomname[0] = data[rname_start:rname_end]
+		rname_start = conn_msg.find("JOIN_CHATROOM".encode(encoding='utf-8')) + 3          #Chatroom Name
+		rname_end = conn_msg.find(b'\n') - 2
+		chatroomname[0] = conn_msg[rname_start:rname_end]
 
-		cname_start = data.find('CLIENT_NAME')+3	    #Client Name		
-		cname_end = data.find('\n',cname_start)-2
-		client[0] = data[cname_start:cname_end]
+		cname_start = conn_msg.find("CLIENT_NAME".encode(encoding='utf-8'))+3	    #Client Name		
+		cname_end = conn_msg.find(b'\n',cname_start)-2
+		client[0] = conn_msg[cname_start:cname_end]
 			
 		#No other parameters to check because of TCP connection
 
-	response = "Joined chatroom"
-	csock.send(response.encode('ascii'))
-	csock.close()
-
+		response = "Joined chatroom"
+		csock.send(response.encode('ascii'))
+		csock.close()
+		break
+	break
