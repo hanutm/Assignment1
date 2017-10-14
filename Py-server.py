680db9c9337bca39f898c3c546a404489a8fd7da
Hanut Mathur <hanut.mathur@live.in>
2017-10-14 14:48:24 +0100

    Basic Server Initialised

--- Py-server.py
+++ Py-server.py
@@ -1,1 +1,24 @@
 ## ChartServer code using sockets
+
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+
+port = 50000
+
+server.bind((host.port))
+
+server.listen(5)
+
+while True:
+	
+	csock, addr = server.accept()
+	
+	print("Connected with %s" % str(addr))
+
+	response = "Joined chatroom"
+	csock.send(response.encode('ascii'))
+	csock.close()
+
