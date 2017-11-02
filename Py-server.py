3f8a03540be616b47bf944f26540e5d7fb71228d
Hanut Mathur <hanut.mathur@live.in>
2017-11-02 00:10:57 +0000

    Working Codes

--- Py-server.py
+++ Py-server.py
@@ -23,8 +23,8 @@ def check_msg(msg):
 
 def join(conn_msg,csock):
 	print('Joiner')
-	gname = conn_msg.find(':'.encode('utf-8'))+2
-	gname_end = conn_msg.find('\n'.encode('utf-8'))-1
+	gname = conn_msg.find('JOIN_CHATROOM:'.encode('utf-8'))+14
+	gname_end = conn_msg.find('\n'.encode('utf-8'))
 	groupname = conn_msg[gname:gname_end]
 
 	cname = conn_msg.find('CLIENT_NAME'.encode('utf-8'))+12
@@ -33,11 +33,13 @@ def join(conn_msg,csock):
 	rID = 0
 	
 	if (groupname.decode('utf-8')) == 'g1' :
-		g1_clients.append(clientname)
+		print('g1')
+		g1_clients.append(clThread.socket)
 		rID = 1001
 	elif groupname == 'g2' :
-		g2_clients.append(clientname)
+		g2_clients.append(clThread.socket)
 		rID = 1002
+	print(g1_clients)
 	#sending ackowledgement
 	response = "JOINED_CHATROOM: ".encode('utf-8') + groupname+ "\n".encode('utf-8')
 	response += "SERVER_IP: \n".encode('utf-8')
@@ -54,51 +56,59 @@ def discon():
 
 def leave(conn_msg,csock):
 	print('leaving')
-	grp_start = conn_msg.find('LEAVE_CHATROOM:'.encode('utf-8')) + 15
-	grp_end = conn_msg.find('\n'.encode('utf-8'), grp_start) - 1
+	grp_start = conn_msg.find('LEAVE_CHATROOM:'.encode('utf-8')) + 16
+	grp_end = conn_msg.find('\n'.encode('utf-8'), grp_start) 
 
 	group_name = conn_msg[grp_start:grp_end]
 
-	response = "LEFT_CHATROOM".encode('utf-8') + groupname + "\n".encode('utf-8')
+	response = "LEFT_CHATROOM".encode('utf-8') + group_name + "\n".encode('utf-8')
 	response += "JOIN_ID".encode('utf-8') + str(clThread.uid).encode('utf-8')
 
-	grpmessage = "CLIENT_NAME:".encode('utf-8') + (self.clientname).encode('utf-8') + "\n".encode('utf-8')
-	grpmessage += "CLIENT_ID:".encode('utf-8') + str(self.uid).encode('utf-8') +"\n".encode('utf-8')
+	grpmessage = "CLIENT_NAME:".encode('utf-8') + (clThread.clientname).encode('utf-8') + "\n".encode('utf-8')
+	grpmessage += "CLIENT_ID:".encode('utf-8') + str(clThread.uid).encode('utf-8') +"\n".encode('utf-8')
 	grpmessage += "LEFT GROUP".encode('utf-8')
-	if group_name == g1:
-		g1_clients.remove(self.clientname)
+	print(i)
+	print(group_name)
+	if (group_name.decode('utf-8')) == 'g1':
+		i = g1_clients.index(clThread.socket)
+		del g1_clients[i]
 		for x in g1_clients:
-			(g1_clients[x].socket).send(chat_text)
-	elif group_name == g2:
-		g2_clients.remove(self.clientname)
+			g1_clients[x].send(chat_text)
+	elif (group_name.decode('utf-8')) == 'g2':
+		i = g1_clients.index(clThread.socket)
+		del g2_clients[i]
 		for x in g2_clients:
-			(g2_clients[x].socket).send(chat_text)
-	print('Responsing')
+			g2_clients[x].send(chat_text)
 	csock.send(response)
 	
 
 def chat(conn_msg,csock):
+	print('chat recvd')
 	chat_msg_start = conn_msg.find('MESSAGE:'.encode('utf-8')) + 9
-	chat_msg_end = conn_msg.find('\n\n'.encode('utf-8'),chat_msg_start) - 1	
+	chat_msg_end = conn_msg.find('\n\n'.encode('utf-8'),chat_msg_start) 
 
 	chat_msg = conn_msg[chat_msg_start:chat_msg_end]
 
-	grp_start = conn_msg.find('CHAT:'.encode('utf-8')) + 5
-	grp_end = conn_msg.find('\n'.encode('utf-8'), grp_start) - 1
+	grp_start = conn_msg.find('CHAT:'.encode('utf-8')) + 6 
+	grp_end = conn_msg.find('\n'.encode('utf-8'), grp_start)
 
 	group_name = conn_msg[grp_start:grp_end]
 	
-	chat_text = 'CHAT:'.encode('utf-8') + chat_msg + '\n'.encode('utf-8')			##change to Room number
-	chat_text += 'CLIENT_NAME:'.encode('utf-8') +str(clThread.uid).encode('utf-8')
-	chat_text += 'MESSAGE: ' + chat_msg.encode('utf-8') 
-	
-	if group_name == b'g1':
-		for x in g1_clients:
-			print(g1_clients[x])
-			(g1_clients[x].socket).send(chat_text)
-	elif group_name == b'g2':
+	chat_text = 'CHAT: '.encode('utf-8') + group_name + '\n'.encode('utf-8')			##change to Room number
+	chat_text += 'CLIENT_NAME: '.encode('utf-8') +str(clThread.clientname.encode('utf-8')) + '\n'.encode('utf-8')
+	chat_text += 'MESSAGE: ' + chat_msg.encode('utf-8')
+	if (group_name.decode('utf-8')) == 'g1':
+		for x in range(len(g1_clients)):
+			i = g1_clients.index(clThread.socket)
+			if x == i:
+				continue
+			g1_clients[x].send(chat_text)
+	elif group_name == 'g2':
 		for x in g2_clients:
-			(g2_clients[x].socket).send(chat_text)
+			i = g2_clients.index(clThread.socket)
+			if x == i:
+				continue
+			g2_clients[x].send(chat_text)
 	
 class client_threads(Thread):
 
--- PyClient.py
+++ PyClient.py
@@ -5,24 +5,26 @@ import sys,os
 from threading import Thread
 
 def join():
-	chatroom = input('Enter Chatroom name to enter')
+	chatroom = input('Enter Chatroom name to enter ')
 
 	conn_msg = "JOIN_CHATROOM:".encode('utf-8') + chatroom.encode('utf-8') + "\n".encode('utf-8')
 	conn_msg += "CLIENT IP: \n".encode('utf-8')
 	conn_msg += "PORT: \n".encode('utf-8')
 	conn_msg += "CLIENT_NAME:".encode('utf-8') + Cname.encode('utf-8') + "\n".encode('utf-8')
+	print(conn_msg)
 	s.send(conn_msg)
-def chat():
-	croom = input('Which room to send message?')
-	chat_message = input('Enter Message to send')
+
+def chat(socket):
+	croom = input('Which room to send message? ')
+	chat_message = input('Enter Message to send: ')
 	msg = "CHAT: ".encode('utf-8') + croom.encode('utf-8') + "\n".encode('utf-8')
 	msg += "JOIN_ID: ".encode('utf-8') + str(jID).encode('utf-8') + "\n".encode('utf-8')
 	msg += "CLIENT_NAME: ".encode('utf-8') + Cname.encode('utf-8') + "\n".encode('utf-8')
 	msg += "MESSAGE: ".encode('utf-8') + chat_message.encode('utf-8') + "\n\n".encode('utf-8')
-	s.send(msg)
+	socket.send(msg)
 
 def leave(s):
-	rTL = input('Room to Leave')
+	rTL = input('Room to Leave ')
 	msg = "LEAVE_CHATROOM: ".encode('utf-8') + rTL.encode('utf-8') + "\n".encode('utf-8')
 	msg += "JOIN_ID: ".encode('utf-8') + jID.encode('utf-8') + "\n".encode('utf-8')
 	msg += "CLIENT_NAME: ".encode('utf-8') + Cname.encode('utf-8') 	
@@ -55,12 +57,12 @@ class Client(Thread):
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
  
 # get local machine name 
-host = input('Enter Hostname')
+host = input('Enter Hostname ')
  
-port = input('Port') 
+port = input('Impprt Port ') 
 # connection to hostname on the port. 
 s.connect((host, int(port)))                                
-Cname = input('Give Client Name')
+Cname = input('Give Client Name ')
 join()
 jID=0
 data = s.recv(1024)
@@ -80,7 +82,7 @@ while(1):
 	print('2. Chat')
 	print('3. Leave')
 	print('4. Disconnect')
-	task = input('?')
+	task = input('')
 	if task == '1':
 		join()
 	elif task == '2':
